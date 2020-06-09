73f55c314fb18af4823f3bfe610a4b2bf8fadeff
happychild8p <happychild8p@gmail.com>
2020-06-09 05:43:22 -0400

    error fixed

--- ops435/quiz/q1-1.py
+++ ops435/quiz/q1-1.py
@@ -9,4 +9,3 @@ child_6to17 = input("What is the number of child aged 6 to 17?")
 ccb_total = (int(ccb_u6) * int(child_u6)) + (int(ccb_6to17) * int(child_6to17))
 
 print("The total Canada Child Benefit for the family is " + str(ccb_total))
-
